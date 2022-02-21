dec87dc74216be0ddc4bd902c314bf4aab5080f0
Henrik Strand <strand@t-k-l.dk>
2022-02-21 22:37:42 +0100

    Added support for shooting range id

--- src/settings.py
+++ src/settings.py
@@ -7,3 +7,7 @@ SIUSDATA_PORT = os.getenv("SIUSDATA_PORT", default=4000)
 # RabbitMQ
 RABBITMQ_DEFAULT_USER = os.getenv("RABBITMQ_DEFAULT_USER", default="guest")
 RABBITMQ_DEFAULT_PASS = os.getenv("RABBITMQ_DEFAULT_PASS", default="guest")
+
+# Shooting Range ID
+# EBA2ED57-BF56-47DE-91F8-DEA032843FE3 is our test range
+SHOOTING_RANGE_ID = os.getenv("SHOOTING_RANGE_ID", default="EBA2ED57-BF56-47DE-91F8-DEA032843FE3")
