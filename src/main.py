df2237e78d8f62774cca8cd6447edc0f276f3122
Henrik Strand <strand@t-k-l.dk>
2022-02-22 00:00:31 +0100

    We are able to process some events from the shooting range

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import asyncio
 import settings
-import time
 
 async def tcp_client():
     reader, writer = await asyncio.streams.open_connection(
@@ -8,25 +7,34 @@ async def tcp_client():
     
     while(True):
         data = await reader.readline()
-        print(f'Received: {data.decode(encoding="iso-8859-1")}')
-        print(f"Received at {time.strftime('%X')}")
+        await message_parser(data.decode(encoding="iso-8859-1"))
 
-async def count_numbers():
-    number = 0
-    while(True):
-        print(f'Number is: {number}')
-        number = number + 1
-        await asyncio.sleep(5)
+async def message_parser(data):
+    eventType = data.split(";")[0]
+    if eventType == "_SHOT":
+        print(f"Recieved SHOT")
+        print(f"{data}")
+    elif eventType == "_GRPH":
+        print(f"Recieved GROUP")
+        print(f"{data}")
+    elif eventType == "_NAME":
+        print(f"Recieved NAME")
+        print(f"{data}")
+    elif eventType == "_PRCH":
+        print(f"Recieved PRACTICE")
+        print(f"{data}")
+    elif eventType == "_SNAT":
+        print(f"Recieved NATION")
+        print(f"{data}")
+    elif eventType == "_TEAM":
+        print(f"Recieved TEAM")
+        print(f"{data}")
 
 async def main():
     task_sius = asyncio.create_task(
-        tcp_client())
-    #task_count_number = asyncio.create_task(count_numbers())
-
-    # Wait until both tasks are completed (should take
-    # around 2 seconds.)
+        tcp_client()
+    )
+    
     await task_sius
-    #await task_count_number
 
 asyncio.run(main())
-#loop.run_forever(tcp_client())
