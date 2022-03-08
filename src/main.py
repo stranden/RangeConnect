2f3d2635c964632f0601afb16e5aeaea32f335b7
Henrik Strand <strand@t-k-l.dk>
2022-03-08 22:21:00 +0100

    Moved SiusMessageParser instance out from while true loop

--- src/main.py
+++ src/main.py
@@ -11,10 +11,11 @@ async def tcp_client():
         reader, writer = await asyncio.streams.open_connection(
             settings.SIUSDATA_HOST, settings.SIUSDATA_PORT)
 
+        SiusMessageParser = services.sius.message_parser.SiusMessageParser()
+        
         while(True):
             data = await reader.readline()
 
-            SiusMessageParser = services.sius.message_parser.SiusMessageParser()
             await SiusMessageParser.message_parser(data.decode(encoding="iso-8859-1"))
 
             #Publish = services.messaging.publisher.Publisher(settings.RABBITMQ_URI, settings.RANGE_TYPE)
