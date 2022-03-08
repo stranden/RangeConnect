1b6c4cf461b2d756607589d4c1725eb8d517576e
Henrik Strand <strand@t-k-l.dk>
2022-03-08 21:32:29 +0100

    Added a little bit of error handling

--- src/main.py
+++ src/main.py
@@ -6,32 +6,36 @@ import util
 from aiologger import Logger
 
 async def tcp_client():
-    await logger.info("Connecting to shooting range")
-    reader, writer = await asyncio.streams.open_connection(
-        settings.SIUSDATA_HOST, settings.SIUSDATA_PORT)
-    
-    while(True):
-        data = await reader.readline()
+    await logger.info(f"RANGE_TYPE is: \"{settings.RANGE_TYPE}\"")
+    if settings.RANGE_TYPE is "sius":
+        await logger.info("Connecting to shooting range")
+        reader, writer = await asyncio.streams.open_connection(
+            settings.SIUSDATA_HOST, settings.SIUSDATA_PORT)
 
-        SiusMessageParser = services.sius.message_parser.SiusMessageParser()
-        await SiusMessageParser.message_parser(data.decode(encoding="iso-8859-1"))
-        
-        #Publish = services.messaging.publisher.Publisher(settings.RABBITMQ_URI, settings.RANGE_TYPE)
-        #await Publish.publish_range_events(message)
+        while(True):
+            data = await reader.readline()
+
+            SiusMessageParser = services.sius.message_parser.SiusMessageParser()
+            await SiusMessageParser.message_parser(data.decode(encoding="iso-8859-1"))
+
+            #Publish = services.messaging.publisher.Publisher(settings.RABBITMQ_URI, settings.RANGE_TYPE)
+            #await Publish.publish_range_events(message)
+    else:
+        await logger.error(f"RANGE_TYPE is NOT supported - value: \"{settings.RANGE_TYPE}\"")
 
 async def main():
     await logger.info("Checking if environment variable 'SHOOTING_RANGE_ID' is valid")
     if util.check_shooting_range_id(settings.SHOOTING_RANGE_ID) == True:
-        await logger.info(f"SHOOTING_RANGE_ID is valid - Value: \"{settings.SHOOTING_RANGE_ID}\"")
+        await logger.info(f"SHOOTING_RANGE_ID is valid - value: \"{settings.SHOOTING_RANGE_ID}\"")
         await logger.info("Creating Streaming task")
 
-        task_sius = asyncio.create_task(
+        task_stream = asyncio.create_task(
             tcp_client()
         )
 
         await logger.info("Executing Streaming task")
-        await task_sius
+        await task_stream
     else:
-        await logger.debug(f"SHOOTING_RANGE_ID is NOT valid - Value: \"{settings.SHOOTING_RANGE_ID}\"")
+        await logger.error(f"SHOOTING_RANGE_ID is NOT valid - value: \"{settings.SHOOTING_RANGE_ID}\"")
 logger = Logger.with_default_handlers(name='RangeConnect')
 asyncio.run(main())
